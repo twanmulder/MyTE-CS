6907340aa8f97e7705e8bdb564bee85ea29f8496
TwanMulder <twan.mulder@gmail.com>
2019-12-06 15:51:37 +0100

    Updated witth very basic styling

--- src/main.js
+++ src/main.js
@@ -1,1 +1,45 @@
+function cssEngine(rule) {
+  var css = document.createElement("style"); // Creates <style></style>
+  css.type = "text/css"; // Specifies the type
+  if (css.styleSheet) css.styleSheet.cssText = rule;
+  // Support for IE
+  else css.appendChild(document.createTextNode(rule)); // Support for the rest
+  document.getElementsByTagName("head")[0].appendChild(css); // Specifies where to place the css
+}
+
+// CSS rules
+var rule = `
+@import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+
+body{
+  font-family: 'Open Sans', sans-serif;
+}
+
+table{
+  font-family: inherit;
+}
+
+.navigationClass li {
+  padding-left: 0;
+}
+
+.navigationClass a {
+  border: 1px solid #415b76;
+  border-bottom: none;
+}
+
+.navigationClass a,
+.navigationClass a span{
+  background: #b3c3d2;
+  border-radius: 4px 4px 0 0;
+  padding: 2px 6px;
+}
+
+.navigationClass a.selectedTabLink,
+.navigationClass a.selectedTabLink span {
+  background: #eef2f5;
+}
+`;
+
 console.log("Changing styling of MyTE");
+cssEngine(rule);
